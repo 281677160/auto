2f230e69c25869d7e877b8f17dafa7d16301efb6
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-27 10:34:40 +0800

    Update autoupdate.lua

--- luasrc/controller/autoupdate.lua
+++ luasrc/controller/autoupdate.lua
@@ -1,5 +1,15 @@
-module("luci.controller.autoupdate",package.seeall)
+module("luci.controller.autoupdate", package.seeall)
 
 function index()
-	entry({"admin","system","autoupdate"},cbi("autoupdate"),_("AutoUpdate"),89)
+    entry({"admin", "system", "autoupdate"}, 
+        cbi("autoupdate"),
+        _("AutoUpdate"), 60).dependent = false
+	
+    entry({"admin", "system", "autoupdate", "do_upgrade"}, 
+        call("action_upgrade"), 
+        nil).leaf = true
+end
+
+function action_upgrade()
+    luci.http.redirect(luci.dispatcher.build_url("admin/system/autoupdate"))
 end
