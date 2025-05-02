6345cb44c2f0bff233277c0eb2045320499c9fc9
281677160 <66651997+281677160@users.noreply.github.com>
2025-05-02 21:40:39 +0800

    Update autoupdate.lua

--- luasrc/controller/autoupdate.lua
+++ luasrc/controller/autoupdate.lua
@@ -40,9 +40,9 @@ function action_upgrade()
     if check_result ~= 0 then
         -- 新增版本文件判断:ml-citation{ref="2,6" data="citationList"}
         if check_version_file() then
-            luci.http.write_json({ success = false, message = "云端没有最新固件,无需更新" })
+            luci.http.write_json({ success = false, message = "云端没有最新固件,无需更新", needUpgrade = false })
         else
-            luci.http.write_json({ success = false, message = "Check update failed" })
+            luci.http.write_json({ success = false, message = "Check update failed", needUpgrade = false })
         end
         return
     end
